edfa0b3551ba08fa4ef0d0bb3880786298140970
SolChanP <qkrthfcks1@naver.com>
2021-09-03 17:07:50 +0900

    [JWT-3] ExpiredJwtException Error Log

--- springboot-jwt/src/main/java/app/jwt/JwtTokenProvider.java
+++ springboot-jwt/src/main/java/app/jwt/JwtTokenProvider.java
@@ -96,7 +96,7 @@ public class JwtTokenProvider {
             Jws<Claims> claims = Jwts.parser().setSigningKey(secretKey).parseClaimsJws(jwtToken);
             return !claims.getBody().getExpiration().before(new Date());
         } catch (ExpiredJwtException e) {
-            log.info(e.getMessage());
+            log.error(e.getMessage());
             return false;
         }
     }
