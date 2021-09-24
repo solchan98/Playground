694ac16bdfc706dc4391a4747407a26fa9d4d02e
SolChanP <qkrthfcks1@naver.com>
2021-09-24 16:05:03 +0900

    [FCM-1] FirebaseCloudMessage 서비스 작성

--- springboot-fcm/src/main/java/com/springbootfcm/service/FirebaseCloudMessageService.java
+++ springboot-fcm/src/main/java/com/springbootfcm/service/FirebaseCloudMessageService.java
@@ -1,12 +1,59 @@
 package com.springbootfcm.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.auth.oauth2.GoogleCredentials;
+import com.google.common.net.HttpHeaders;
+import com.google.gson.JsonParseException;
+import com.springbootfcm.FcmMessage;
+import lombok.RequiredArgsConstructor;
+import okhttp3.*;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.util.List;
 
+@Component
+@RequiredArgsConstructor
 public class FirebaseCloudMessageService {
+
+    private final String API_URL = "https://fcm.googleapis.com/v1/projects/test-84752/messages:send";
+    private final ObjectMapper objectMapper;
+
+    public void sendMessageTo(String targetToken, String title, String body) throws IOException {
+        String message = makeMessage(targetToken, title, body);
+
+        OkHttpClient client = new OkHttpClient();
+        RequestBody requestBody = RequestBody.create(message,
+                MediaType.get("application/json; charset=utf-8"));
+        Request request = new Request.Builder()
+                .url(API_URL)
+                .post(requestBody)
+                .addHeader(HttpHeaders.AUTHORIZATION, "Bearer " + getAccessToken())
+                .addHeader(HttpHeaders.CONTENT_TYPE, "application/json; UTF-8")
+                .build();
+
+        Response response = client.newCall(request).execute();
+
+        System.out.println(response.body().string());
+    }
+
+    private String makeMessage(String targetToken, String title, String body) throws JsonParseException, JsonProcessingException {
+        FcmMessage fcmMessage = FcmMessage.builder()
+                .message(FcmMessage.Message.builder()
+                    .token(targetToken)
+                    .notification(FcmMessage.Notification.builder()
+                            .title(title)
+                            .body(body)
+                            .image(null)
+                            .build()
+                    ).build()).validateOnly(false).build();
+
+        return objectMapper.writeValueAsString(fcmMessage);
+    }
+
+
     private String getAccessToken() throws IOException {
         String firebaseConfigPath = "firebase/firebase_service_key.json";
 
