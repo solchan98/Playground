0d512d5f2dbcad38f5ac7931157220451bcd596d
SolChanP <qkrthfcks1@naver.com>
2021-09-24 15:52:18 +0900

    [FCM-1] FCM Message 도메인 작성

---
+++ springboot-fcm/src/main/java/com/springbootfcm/FcmMessage.java
@@ -0,0 +1,31 @@
+package com.springbootfcm;
+
+import com.google.firebase.messaging.Notification;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@AllArgsConstructor
+@Getter
+public class FcmMessage {
+    private boolean validateOnly;
+    private Message message;
+
+    @Builder
+    @AllArgsConstructor
+    @Getter
+    public static class Message {
+        private Notification notification;
+        private String token;
+    }
+
+    @Builder
+    @AllArgsConstructor
+    @Getter
+    public static class Notification {
+        private String title;
+        private String body;
+        private String image;
+    }
+}
