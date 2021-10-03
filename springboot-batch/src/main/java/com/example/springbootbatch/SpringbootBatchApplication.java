80c8ae97ab7b76cf15e8fcab9d7f90bbaa665fc4
SolChanP <qkrthfcks1@naver.com>
2021-10-03 19:33:58 +0900

    [BATCH-1] 기본적인 작업과 스텝 생성 및 API요청으로 Batch 수행해보기

---
+++ springboot-batch/src/main/java/com/example/springbootbatch/SpringbootBatchApplication.java
@@ -0,0 +1,14 @@
+package com.example.springbootbatch;
+
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBatchApplication.class, args);
+    }
+
+}
