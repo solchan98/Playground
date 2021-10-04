9b3c482a41d8cbd6d5268273abc3006901e03128
SolChanP <qkrthfcks1@naver.com>
2021-10-04 15:06:03 +0900

    [BATCH-2] Scheduler를 적용하여 Batch Job 주기(2초)적으로 실행하기

--- springboot-batch/src/main/java/com/example/springbootbatch/SpringbootBatchApplication.java
+++ springboot-batch/src/main/java/com/example/springbootbatch/SpringbootBatchApplication.java
@@ -1,10 +1,11 @@
 package com.example.springbootbatch;
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SpringbootBatchApplication {
 
     public static void main(String[] args) {
