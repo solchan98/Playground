b86a7c69e767cfe341f0ba9a4fc1078a55b70130
solchan98 <dev.sol0427@gmail.com>
2021-11-25 18:23:48 +0900

    feat: JUnit5 기본적인 사용법 및 계좌 시나리오

--- java-test/src/main/java/Main.java
+++ java-test/src/main/java/Main.java
@@ -3,3 +3,36 @@ public class Main {
 
     }
 }
+
+/** 2 */
+class Account {
+    private String customerName;
+    private String accountNumber;
+    private Long balance;
+
+    public Account(String customerName, String accountNumber, Long balance) {
+        this.customerName = customerName;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+
+    public String getCustomerName() {
+        return this.customerName;
+    }
+
+    public String getAccountNumber() {
+        return this.accountNumber;
+    }
+
+    public Long getBalance() {
+        return this.balance;
+    }
+
+    public void withDraw(Long balance) {
+        if(this.balance - balance < 0) {
+            throw new RuntimeException("잔고가 부족합니다.");
+        } else {
+            this.balance -= balance;
+        }
+    }
+}
