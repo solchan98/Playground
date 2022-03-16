bc9f7a67bc2960125a2ce4e0ac87cfb18796c17f
solchan98 <dev.sol0427@gmail.com>
2022-03-16 18:12:47 +0900

    feat: JWT 발급을 위한 Filter 및 Provider 작성과 Filter에서의 예외 처리를 위한 Handler작성 및 이를 위한 SecurityConfig 설정 수정

---
+++ spring-jwt/src/main/java/com/example/springjwt/global/jwt/exception/CustomAuthenticationEntryPoint.java
@@ -0,0 +1,41 @@
+package com.example.springjwt.global.jwt.exception;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Getter
+@Component
+public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException {
+        String exception = (String) request.getAttribute("exception");
+        setResponse(response);
+        // exception을 Messaage 객체로 해서 error로
+        response.getWriter().print(convertObjectToJson(exception));
+    }
+
+    private void setResponse(HttpServletResponse response) {
+        response.setStatus(HttpStatus.FORBIDDEN.value());
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+    }
+
+    public String convertObjectToJson(Object object) throws JsonProcessingException {
+        if (object == null) {
+            return null;
+        }
+        return mapper.writeValueAsString(object);
+    }
+}
