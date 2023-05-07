882e50e810d36014827f3a5563766d810bc7c561
solchan98 <dev.sol0427@gmail.com>
2023-05-08 08:54:06 +0900

    [MYBATIS-SPRING-BOOT-STARTER] feat: BoardRepository Board 수정 메서드 구현 UpdateBoard VO 작성

--- mybatis/mybatis-spring-boot-starter/src/main/java/com/example/mybatisspringbootstarter/repository/BoardRepository.java
+++ mybatis/mybatis-spring-boot-starter/src/main/java/com/example/mybatisspringbootstarter/repository/BoardRepository.java
@@ -1,10 +1,7 @@
 package com.example.mybatisspringbootstarter.repository;
 
 import com.example.mybatisspringbootstarter.Board;
-import org.apache.ibatis.annotations.Insert;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Param;
-import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.*;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -18,9 +15,7 @@ public interface BoardRepository {
     String ALL_PARAM_FIELD_OF_BOARD =
             "#{board.id}, #{board.title}, #{board.content}, #{board.author}, #{board.createdAt}, #{board.updatedAt}";
 
-    @Insert(
-            "insert into board values(" + ALL_PARAM_FIELD_OF_BOARD + ")"
-    )
+    @Insert("insert into board values(" + ALL_PARAM_FIELD_OF_BOARD + ")")
     void create(@Param("board") Board board);
 
     @Select("select " + ALL_FIELD_OF_BOARD + " from board")
@@ -28,4 +23,9 @@ public interface BoardRepository {
 
     @Select("select " + ALL_FIELD_OF_BOARD + " from board where id = #{id}")
     Board findById(@Param("id") String id);
+
+    @Update("update board " +
+            "set title = #{updateBoard.title}, content = #{updateBoard.content}, updated_at = #{updateBoard.updatedAt} " +
+            "where id = #{updateBoard.id}")
+    void update(@Param("updateBoard") UpdateBoard updateBoard);
 }
