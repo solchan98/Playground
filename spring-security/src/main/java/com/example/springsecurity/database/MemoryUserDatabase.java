a94e7690f3b271ac16dacdbf5be84b856fbeec0b
solchan98 <dev.sol0427@gmail.com>
2023-05-15 08:58:32 +0900

    [SpringSecurity] feat: 메모리 디비에 초기 관리자 계정 넣기 어플리케이션이 실행될 때, amdin role를 가지는 sol user 데이터 넣도록 수정

--- spring-security/src/main/java/com/example/springsecurity/database/MemoryUserDatabase.java
+++ spring-security/src/main/java/com/example/springsecurity/database/MemoryUserDatabase.java
@@ -1,10 +1,21 @@
 package com.example.springsecurity.database;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public final class MemoryUserDatabase {
     public static final Map<String, User> userDB = new HashMap<>();
+
+    static {
+        String username = "a3601804-496f-456f-8bc8-4d1d7511d7d1";
+        String userId = "sol";
+        String password = "$2a$10$9ZVmRiKcPDDO.lCiSBd8TO6AcFekug1WDQjjj1FoU9fllBEwt4FUy";
+        List<String> roles = List.of("admin");
+
+        userDB.put(username, new User(username, userId, password, roles, LocalDateTime.MIN, true));
+    }
 
     private MemoryUserDatabase() {}
 }
