ee8a1b12a77c4a0a6922754b46b1ea36990e2251
solchan98 <dev.sol0427@gmail.com>
2023-05-15 13:44:33 +0900

    [SpringSecurity] feat: MemoryUserDatabase 기본 유저 정보 추가 고객 계정, 판매자 계정, 관리자 계정 각 1개씩 추가

--- spring-security/src/main/java/com/example/springsecurity/database/MemoryUserDatabase.java
+++ spring-security/src/main/java/com/example/springsecurity/database/MemoryUserDatabase.java
@@ -9,11 +9,28 @@ public final class MemoryUserDatabase {
     public static final Map<String, User> userDB = new HashMap<>();
 
     static {
-        String userId = "sol";
-        String password = "$2a$10$9ZVmRiKcPDDO.lCiSBd8TO6AcFekug1WDQjjj1FoU9fllBEwt4FUy";
-        List<String> roles = List.of("admin");
+        // default customer user
+        String userIdOfCustomer = "sol";
+        String passwordOfCustomer = "$2a$10$9ZVmRiKcPDDO.lCiSBd8TO6AcFekug1WDQjjj1FoU9fllBEwt4FUy";
+        List<String> rolesOfCustomer = List.of("customer");
 
-        userDB.put(userId, new User(userId, password, roles, LocalDateTime.MIN, true));
+        // default seller user
+        String userIdOfSeller = "seller";
+        String passwordOfSeller = "$2a$10$6uN6qaVxfUxHJOn917TdwezefDbhFABHbeAtAX2V9jA35OiBzuUOa"; //seller
+        List<String> rolesOfSeller = List.of("seller", "customer");
+
+        // default admin user
+        String userIdOfAdmin = "admin";
+        String passwordOfAdmin = "$2a$12$FTQFZ96f18OuLbt08eNjYuA.HimNhH6CcR.Y8uZpmPuDRQoKEpLti"; // admin
+        List<String> rolesOfAdmin = List.of("admin", "seller", "customer");
+
+        userDB.putAll(
+                Map.of(
+                        userIdOfAdmin, new User(userIdOfAdmin, passwordOfAdmin, rolesOfAdmin, LocalDateTime.MIN, true),
+                        userIdOfSeller, new User(userIdOfSeller, passwordOfSeller, rolesOfSeller, LocalDateTime.MIN, true),
+                        userIdOfCustomer, new User(userIdOfCustomer, passwordOfCustomer, rolesOfCustomer, LocalDateTime.MIN, true)
+                )
+        );
     }
 
     private MemoryUserDatabase() {}
