0c9ba15d3ae7e1ca435c36f5bc0e5c41bc3ac4e7
solchan98 <dev.sol0427@gmail.com>
2023-05-15 14:06:02 +0900

    [SpringSecurity] feat: 권한에 따른 로그인 후의 Redirect 설정 구현

--- spring-security/src/main/java/com/example/springsecurity/prensentation/config/SessionLoginSuccessHandler.java
+++ spring-security/src/main/java/com/example/springsecurity/prensentation/config/SessionLoginSuccessHandler.java
@@ -3,14 +3,27 @@ package com.example.springsecurity.prensentation.config;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 import java.io.IOException;
+import java.util.Collection;
 
 public class SessionLoginSuccessHandler implements AuthenticationSuccessHandler {
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException {
-        // TODO: 권한에 따른 redirect 페이지 처리하기
-        response.sendRedirect("/");
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        if (authorities.contains(new SimpleGrantedAuthority("admin"))) {
+            response.sendRedirect("/admin");
+            return;
+        }
+
+        if (authorities.contains(new SimpleGrantedAuthority("seller"))) {
+            response.sendRedirect("/seller");
+            return;
+        }
+
+        response.sendRedirect("/customer");
     }
 }
