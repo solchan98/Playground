e13138186f31949499b6c6078ebaa4814136efd1
solchan98 <dev.sol0427@gmail.com>
2023-05-15 14:09:03 +0900

    [SpringSecurity] feat: 각 권한에 따른 GetMapping 메서드 구현 각 권한에 따른 메서드는 단순한 텍스트를 가지는 HTML을 응답하도록 구현

--- spring-security/src/main/java/com/example/springsecurity/prensentation/MainController.java
+++ spring-security/src/main/java/com/example/springsecurity/prensentation/MainController.java
@@ -19,13 +19,60 @@ public class MainController {
             HttpServletResponse response,
             @AuthenticationPrincipal UserDetails userDetails
     ) throws IOException {
-
         response.setContentType(MediaType.TEXT_HTML_VALUE);
         response.setCharacterEncoding(StandardCharsets.UTF_8.name());
 
         PrintWriter writer = response.getWriter();
         writer.write("<html lang=\"kr\">");
         writer.write("<body>");
+        writer.write("<div>" +  userDetails.getUsername() + "님 안녕하세요!" + "</div>");
+        writer.write("<html>");
+    }
+
+    @GetMapping("/admin")
+    public void adminIntro(
+            HttpServletResponse response,
+            @AuthenticationPrincipal UserDetails userDetails
+    ) throws IOException {
+        response.setContentType(MediaType.TEXT_HTML_VALUE);
+        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+
+        PrintWriter writer = response.getWriter();
+        writer.write("<html lang=\"kr\">");
+        writer.write("<body>");
+        writer.write("<h3>관리자 페이지</h3>");
+        writer.write("<div>" +  userDetails.getUsername() + " 관리자님 안녕하세요!" + "</div>");
+        writer.write("<html>");
+    }
+
+    @GetMapping("/seller")
+    public void sellerIntro(
+            HttpServletResponse response,
+            @AuthenticationPrincipal UserDetails userDetails
+    ) throws IOException {
+        response.setContentType(MediaType.TEXT_HTML_VALUE);
+        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+
+        PrintWriter writer = response.getWriter();
+        writer.write("<html lang=\"kr\">");
+        writer.write("<body>");
+        writer.write("<h3>판매자 페이지</h3>");
+        writer.write("<div>" +  userDetails.getUsername() + " 판매자님 안녕하세요!" + "</div>");
+        writer.write("<html>");
+    }
+
+    @GetMapping("/customer")
+    public void customerPage(
+            HttpServletResponse response,
+            @AuthenticationPrincipal UserDetails userDetails
+    ) throws IOException {
+        response.setContentType(MediaType.TEXT_HTML_VALUE);
+        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+
+        PrintWriter writer = response.getWriter();
+        writer.write("<html lang=\"kr\">");
+        writer.write("<body>");
+        writer.write("<h3>구매자 페이지</h3>");
         writer.write("<div>" +  userDetails.getUsername() + "님 안녕하세요!" + "</div>");
         writer.write("<html>");
     }
