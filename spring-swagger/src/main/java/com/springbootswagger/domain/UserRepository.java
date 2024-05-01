bfb366b38871870ac011aa8499b1ccf7580ae49d
solchan98 <dev.sol0427@gmail.com>
2024-05-01 16:26:00 +0900

    feat: swagger 예제 추가

---
+++ spring-swagger/src/main/java/com/springbootswagger/domain/UserRepository.java
@@ -0,0 +1,10 @@
+package com.springbootswagger.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    User save(User account);
+    Optional<User> findByEmail(String email);
+}
